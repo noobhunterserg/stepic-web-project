d6fe10731302a2ec3d2bd4cc4258023a9b70f05d
noobhunterserg <masterserg321@gmail.com>
2017-07-04 21:10:11 +0300

    2nd day changes

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/home/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo rm /etc/nginx/sites-enabled/default
