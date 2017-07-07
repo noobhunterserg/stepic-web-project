2d82d957190d5a9adc7fb1e23783462fda22dbea
noobhunterserg <masterserg321@gmail.com>
2017-07-07 16:32:16 +0300

    little change

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 def application(env, start_response):
-    # string = env
-    # ind = string.index('?')
-    # parsing_line=string[ind+1:]
-    # spl = parsing_line.split('&')
-    # body = '\n'.join(spl)
-    body = '2'
+    string = env
+    ind = string.index('?')
+    parsing_line=string[ind+1:]
+    spl = parsing_line.split('&')
+    body = '\n'.join(spl)
+    
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
