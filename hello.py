fd13a4599a56864480835780f8955ee2cc0d5254
noobhunterserg <masterserg321@gmail.com>
2017-07-07 16:14:44 +0300

    first adding gunicorn

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+def application(env, start_response):
+    string = env
+    ind = string.index('?')
+    parsing_line=string[ind+1:]
+    spl = parsing_line.split('&')
+    body = '\n'.join(spl)
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+
+    return (body)
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/serg/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/serg/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo gunicorn hello:application 
