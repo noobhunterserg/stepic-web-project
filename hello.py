9998ea25c9a154b00bab1e5d97d4f798e79849d7
noobhunterserg <masterserg321@gmail.com>
2017-07-08 14:36:32 +0300

    body = list + ascii

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 def application(env, start_response):
-    body = [bytes(i + '\n') for i in env['QUERY_STRING'].split('&')]
+    body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
