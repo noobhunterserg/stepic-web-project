762b2eee86b178bc2b26d69eda0f553a42dffd37
noobhunterserg <masterserg321@gmail.com>
2017-07-08 14:12:59 +0300

    first working unicorn

--- hello.py
+++ hello.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 def application(env, start_response):
-    string = env
-    ind = string.index('?')
-    parsing_line=string[ind+1:]
-    spl = parsing_line.split('&')
+    string = env['QUERY_STRING']
+    spl = string.split('&')
     body = '\n'.join(spl)
-    
+
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
