859ad786fcc91470fb6b808da7b3dc4b6cbd02f5
noobhunterserg <masterserg321@gmail.com>
2017-07-19 18:29:21 +0300

    added models

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,5 @@
 sudo /etc/init.d/mysql start
+mysql -uroot -e 'DROP DATABASE QA;'
 mysql -uroot -e 'CREATE DATABASE QA;'
-mysql -uroot -e 'CREATE USER serguser@localhost IDENTIFIED BY 'topsecret';' 
+mysql -uroot -e "CREATE USER serguser@localhost IDENTIFIED BY 'topsecret';"
 mysql -uroot -e 'GRANT ALL ON QA.* TO serguser@localhost;'
