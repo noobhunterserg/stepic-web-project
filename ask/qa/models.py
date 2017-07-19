fcc13379a57ec530d6333a0f1753606282fdf406
noobhunterserg <masterserg321@gmail.com>
2017-07-19 19:10:15 +0300

    little changes int mysql.sh

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,13 +11,13 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length = 255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(auto_now = True)
+    rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User, related_name = 'q_author')
     likes = models.ManyToManyField(User, related_name = 'q_likes')
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateTimeField(auto_now = True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User, related_name ='a_author')
--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,7 @@
 sudo /etc/init.d/mysql start
-mysql -uroot -e 'DROP DATABASE QA;'
-mysql -uroot -e 'CREATE DATABASE QA;'
-mysql -uroot -e "CREATE USER serguser@localhost IDENTIFIED BY 'topsecret';"
-mysql -uroot -e 'GRANT ALL ON QA.* TO serguser@localhost;'
+mysql -uroot -p -e 'DROP DATABASE QA;'
+mysql -uroot -p -e 'CREATE DATABASE QA;'
+mysql -uroot -p -e "CREATE USER serguser@localhost IDENTIFIED BY 'topsecret';"
+mysql -uroot -p -e 'GRANT ALL ON QA.* TO serguser@localhost;'
+python3 ask/manage.py makemigrations
+python3 ask/manage.py migrate
